5a1cb032537754255a48fc16ded58b147507af52
KirillBogdanov1 <144319949+KirillBogdanov1@users.noreply.github.com>
2024-01-19 12:00:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 from pyscript import document
 
 
-def translate_english(event,event3):
+def translate_english(event):
     input_text = document.querySelector("#zn1")
     input_text2 = document.querySelector("#zn2")
     zn1 = input_text.value
-    zn2 = input_text.value
+    zn2 = input_text2.value
     output_div = document.querySelector("#output")
-    output_div.innerText =zn1+zn2 
+    output_div.innerText =int(zn1)+int(zn2) 
