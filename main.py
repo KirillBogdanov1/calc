ecff1d634fed72bf3ddc18a672e7246fc2b5e042
KirillBogdanov1 <144319949+KirillBogdanov1@users.noreply.github.com>
2024-01-19 10:16:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import arrr
+
 from pyscript import document
 
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
+def translate_english(event,event2):
+    input_text = document.querySelector("#zn1")
+    input_text2 = document.querySelector("#zn2")
+    zn1 = input_text.value
+    zn2 = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+    output_div.innerText =zn1+zn2 
